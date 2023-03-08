c5d20585982e8db015a6a71c1c7e70e4eb30e33f
POluwasemilore <popoola.i.oluwasemilore@gmail.com>
2023-03-08 14:54:42 +0100

    NodeJS Assignment

--- Crypto.js
+++ Crypto.js
@@ -1,7 +1,7 @@
 var crypto = require('crypto');
 
-var mykey = createCipheriv('gjcffjhc', 'mypassword');
-var mystr = mykey.update('abc', 'utf8', 'hex')
-mystr += mykey.final('hex');
+var mykey = crypto.createCipher('aes-128-cbc', 'mypassword');
+var mystr = mykey.update('abc', 'utf8', 'bin')
+mystr += mykey.final('bin');
 
-console.log(mystr); //34feb914c099df25794bf9ccb85bea72
+console.log(mystr); 
